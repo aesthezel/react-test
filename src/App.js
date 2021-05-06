6263d59b50b62225ecd10d190048eac1d0aa0576
David Pino <aesthezel@gmail.com>
2021-05-06 04:58:50 -0700

    Added React boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Message from './components/Message.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <Message message='Este es un parametro'></Message>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+const Message = (props) => {
+    console.log(props);
+    return <p>{props.message}</p>
+}
+
+export default Message;
